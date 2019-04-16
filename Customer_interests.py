e2adf539f2a2a23e89b8813700454c4407804e26
yinliang344 <1414737531@qq.com>
2019-04-16 19:28:05 +0800

    over time

--- Customer_interests.py
+++ Customer_interests.py
@@ -7,27 +7,23 @@
 #输入： 第1行为n代表用户的个数 第2行为n个整数，第i个代表用户标号为i的用户对某类文章的喜好度
 # 第3行为一个正整数q代表查询的组数  第4行到第（3+q）行，每行包含3个整数l,r,k代表一组查询，即标号为l<=i<=r的用户中对这类文章喜好值为k的用户的个数。
 # 数据范围n <= 300000,q<=300000 k是整型
-#输出：一共q行，每行一个整数代表喜好值为k的用户的个数
+#输出：一共q行，每行一个整数代表喜好值为k的用户的个数\
+
+#逻辑没问题，牛客网超时，应该可以用数据结构优化一下
 
 import sys
 
-def find(num,search,Q):
-    res = [0]*len(search)
-    for i in range(Q):
-        temp = search[i]
-        temp1 = num[temp[0]-1:temp[1]]
-        res[i] = temp1.count(temp[2])
+def find(num,search):
+    temp = num[search[0]-1:search[1]]
+    res = str(temp.count(search[2]))
     return res
 
 
 if __name__=="__main__":
     N = int(input())
-    num = list(map(int,input().split()))
+    num = list(map(int, input().split()))
     Q = int(input().strip())
-    search = []
     for i in range(Q):
         temp = list(map(int, input().split()))
-        search.append(temp)
-    res = find(num,search,Q)
-    for i in range(Q):
-        print(res[i])
+        res = find(num,temp)
+        print(res)
